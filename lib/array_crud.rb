0d5f0a3edb741aaea31ab109216204c1deeb1e16
cool-changelog-3932 <ruizj3@uw.edu>
2019-10-13 20:28:31 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -38,13 +38,20 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array[0]
 
 end
 
 def retrieve_last_element_from_array(array)
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array[-1]
 
 end
 
 def update_element_from_index(array, index_number, element)
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  index_number = 4
+  array[index_number] = "totally"
 
 end
