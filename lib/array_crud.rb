8231512af31b6e865c2c10099bc390c06a4af234
cool-changelog-3932 <ruizj3@uw.edu>
2019-10-13 20:19:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,9 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-
+  array = [61,62,63,64]
+  element = 65
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
